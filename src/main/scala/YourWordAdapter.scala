8b84cae5d6b1acb00b927091553c8639026013fe
Sonny Malick <sonnymalick@Sonnys-Air.fios-router.home>
2019-03-04 21:22:48 -0500

    test2

--- src/main/scala/YourWordAdapter.scala
+++ src/main/scala/YourWordAdapter.scala
@@ -1,27 +1,8 @@
 package words
 
-class YourWordAdapter(line: String) extends WordInterface {
+class YourWordAdapter(word: String, sounds: List[String]) extends WordInterface {
 
-    
-    def get_sounds() : List[String] = {
-        var arr = line.split(" ")
-        var word: String = arr(0)
-        var l: List[String] = List()
-        for (x <- 2 to arr.length-1) {
-                l = l:+arr(x)
-        }
-        return l
-    }
-    
-    def get_word() : String = {
-        var arr = line.split(" ")
-        var the_word: String = arr(0)
-        return the_word
-    }
-    
-    
-    def sounds(): String = {
-        var sounds : List[String] = get_sounds()
+    def sounds(): String = { 
         var newstr: String = "";
         for (x <- 0 to sounds.length-1) {
             if(x == sounds.length-1) {
@@ -35,7 +16,7 @@ class YourWordAdapter(line: String) extends WordInterface {
         return newstr    
     }
       def spelling() : String = { 
-        return this.get_word().toLowerCase()    
+        return this.word.toLowerCase()    
     }
     
 }
